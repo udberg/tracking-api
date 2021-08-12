36d458c2190a34fa1fcc2f75eecf655a47893e2a
udberg <johnuduak066@gmail.com>
2021-08-12 23:08:30 +0100

    Create postgre database and migrate

---
+++ db/seeds.rb
@@ -0,0 +1,17 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Unit.create(title: 'Weight')
+Unit.create(title: 'Energy')
+Unit.create(title: 'Energy burned')
+Unit.create(title: 'Fats')
+Unit.create(title: 'Saturated fats')
+Unit.create(title: 'Carbonhydrates')
+Unit.create(title: 'Sugar')
+Unit.create(title: 'Protein')
+Unit.create(title: 'Water')
