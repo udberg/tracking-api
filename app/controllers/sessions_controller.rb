c9a70c0c205751ef14836f3a511bf03ae42f7396
udberg <johnuduak066@gmail.com>
2021-08-23 13:40:57 +0100

    Fix linter errors

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,14 @@
 class SessionsController < ApplicationController
   skip_before_action :authorized
 
-def create
-    @user = User.find_by(name: params[:name])
+  def create
+  @user = User.find_by(name: params[:name])
 
-      if @user&.authenticate(params[:password])
+    if @user&.authenticate(params[:password])
         token = encode_token({ user_id: @user.id })
         render json: { user: @user, token: token }
       else
         render json: { error: 'Invalid username or password' }, status: :unprocessable_entity
     end
-end
+   end
 end
